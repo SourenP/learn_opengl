1e249ae86ba01626b3b751c6869dd4c11be8c2ca
souren <souren@papazian.info>
2020-03-18 23:45:21 -0700

    Move shaders into their own files

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 
 #include <GLFW/glfw3.h>
 
+#include <fstream>
 #include <iostream>
+#include <math.h>
+#include <sstream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -11,25 +14,21 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char* vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                   "}\n\0";
-
 int shaderProgram;
 unsigned int VBO, VAO, EBO;
 
 // build and compile our shader program
 void load_shaders() {
     // vertex shader
+    // read in from file
+    std::ifstream input_v("vert.glsl");
+    std::stringstream sstr_v;
+    while (input_v >> sstr_v.rdbuf()) {
+    };
+    std::string str_v = sstr_v.str();
+    char* vertexShaderSource = new char[str_v.length() + 1];
+    std::strcpy(vertexShaderSource, str_v.c_str());
+    // create shader and compile
     int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
@@ -41,7 +40,17 @@ void load_shaders() {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
+
     // fragment shader
+    // read in from file
+    std::ifstream input_f("frag.glsl");
+    std::stringstream sstr_f;
+    while (input_f >> sstr_f.rdbuf()) {
+    };
+    std::string str_f = sstr_f.str();
+    char* fragmentShaderSource = new char[str_f.length() + 1];
+    std::strcpy(fragmentShaderSource, str_f.c_str());
+    // create shader and compile
     int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
@@ -51,6 +60,7 @@ void load_shaders() {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
+
     // link shaders
     shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
@@ -146,7 +156,14 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw our first triangle
+        // pass data to shaders
+        glUseProgram(shaderProgram);
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+        // draw our first vertices
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
