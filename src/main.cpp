d4d06cc50e0dfbe52eff9df683d6ae6d5ba485ee
souren <souren@papazian.info>
2020-03-25 23:28:37 -0700

    Add Coordinate Systems and many 3d rotating cubes

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // settings
-const unsigned int SCR_WIDTH = 400;
-const unsigned int SCR_HEIGHT = 400;
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;
 
 unsigned int VBO, VAO, EBO;
 unsigned int texture1, texture2;
@@ -28,15 +28,43 @@ Shader* shader;
 // set up vertex data (and buffer(s)) and configure vertex attributes
 void setup_vertices() {
     float vertices[] = {
-        // positions  // colors   // texture coords
-        0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
-        0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-        -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+        // pos (3), color (3), texture (2)
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f,  0.0f,  0.0f,  0.0f, 0.5f,  -0.5f, -0.5f, 1.0f,
+        0.0f,  0.0f,  1.0f,  0.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 0.0f,  0.0f,  1.0f,  1.0f,
+        0.5f,  0.5f,  -0.5f, 1.0f, 0.0f,  0.0f,  1.0f,  1.0f, -0.5f, 0.5f,  -0.5f, 1.0f,
+        0.0f,  0.0f,  0.0f,  1.0f, -0.5f, -0.5f, -0.5f, 1.0f, 0.0f,  0.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f, 0.5f,  0.0f, 1.0f,  0.0f,  0.0f,  0.0f, 0.5f,  -0.5f, 0.5f,  0.0f,
+        1.0f,  0.0f,  1.0f,  0.0f, 0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  0.0f,  1.0f,  1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  0.0f,  1.0f,  1.0f, -0.5f, 0.5f,  0.5f,  0.0f,
+        1.0f,  0.0f,  0.0f,  1.0f, -0.5f, -0.5f, 0.5f,  0.0f, 1.0f,  0.0f,  0.0f,  0.0f,
+
+        -0.5f, 0.5f,  0.5f,  0.0f, 0.0f,  1.0f,  1.0f,  0.0f, -0.5f, 0.5f,  -0.5f, 0.0f,
+        0.0f,  1.0f,  1.0f,  1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  1.0f,  0.0f,  1.0f, -0.5f, -0.5f, 0.5f,  0.0f,
+        0.0f,  1.0f,  0.0f,  0.0f, -0.5f, 0.5f,  0.5f,  0.0f, 0.0f,  1.0f,  1.0f,  0.0f,
+
+        0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  1.0f,  1.0f,  0.0f, 0.5f,  0.5f,  -0.5f, 0.0f,
+        1.0f,  1.0f,  1.0f,  1.0f, 0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,  1.0f,  0.0f,  1.0f,
+        0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,  1.0f,  0.0f,  1.0f, 0.5f,  -0.5f, 0.5f,  0.0f,
+        1.0f,  1.0f,  0.0f,  0.0f, 0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  1.0f,  1.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f,  1.0f,  0.0f,  1.0f, 0.5f,  -0.5f, -0.5f, 1.0f,
+        0.0f,  1.0f,  1.0f,  1.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  1.0f,  1.0f,  0.0f,
+        0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  1.0f,  1.0f,  0.0f, -0.5f, -0.5f, 0.5f,  1.0f,
+        0.0f,  1.0f,  0.0f,  0.0f, -0.5f, -0.5f, -0.5f, 1.0f, 0.0f,  1.0f,  0.0f,  1.0f,
+
+        -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,  0.0f,  0.0f,  1.0f, 0.5f,  0.5f,  -0.5f, 1.0f,
+        1.0f,  0.0f,  1.0f,  1.0f, 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  0.0f,  1.0f,  0.0f, -0.5f, 0.5f,  0.5f,  1.0f,
+        1.0f,  0.0f,  0.0f,  0.0f, -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,  0.0f,  0.0f,  1.0f,
+
     };
+
+    // this is not being used right now...
     unsigned int indices[] = {
         0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
+        1, 2, 3, // second triangle
     };
 
     glGenVertexArrays(1, &VAO);
@@ -130,14 +158,35 @@ void setup_shaders() {
     shader->setInt("texture2", 1);                                // or with shader class
 }
 
-// apply transfomrations
-void apply_trans() {
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-    trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-    unsigned int transformLoc = glGetUniformLocation(shader->ID, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+void draw() {
+    // create transformations
+    // view
+    glm::mat4 view = glm::mat4(1.0f);
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    shader->setMat4("view", view);
+    // projection
+    glm::mat4 projection;
+    projection =
+        glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    shader->setMat4("projection", projection);
+
+    // render boxes
+    glBindVertexArray(VAO);
+    glm::vec3 cubePositions[] = {glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+                                 glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+                                 glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+                                 glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+                                 glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
+    for (unsigned int i = 0; i < 10; i++) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[i]);
+        float angle = 20.0f * i;
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(40.0f) + angle,
+                            glm::vec3(1.0f, 0.3f, 0.5f));
+        shader->setMat4("model", model);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 }
 
 int main() {
@@ -165,6 +214,8 @@ int main() {
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     // setup
     setup_vertices();
     setup_textures();
@@ -176,7 +227,7 @@ int main() {
 
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // bind Texture
         glActiveTexture(GL_TEXTURE0);
@@ -186,9 +237,7 @@ int main() {
 
         // render container
         shader->use();
-        apply_trans();
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        draw();
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
